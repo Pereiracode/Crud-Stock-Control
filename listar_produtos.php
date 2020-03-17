69492a4dd98d04253006aeef158fcf3d024d4cbc
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-03-17 15:36:31 -0300

    Atualizando produtos

--- listar_produtos.php
+++ listar_produtos.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <script src="https://kit.fontawesome.com/362ff39427.js" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -19,6 +20,7 @@
       <th scope="col">Categoria</th>
       <th scope="col">Quantidade</th>
       <th scope="col">Fornecedor</th>
+      <th scope="col">Ação</th>
     </tr>
   </thead>
   <tbody>
@@ -45,7 +47,7 @@
         <td><?php echo $categoria ?></td>
         <td><?php echo $quantidade ?></td>
         <td><?php echo $fornecedor ?></td>
-
+        <td><a class="btn btn-warning btn-sm" style="color:#fff;" href="editar_produtos.php?id=<?php echo $id_estoque?>" role="button"><i class="far fa-edit"></i>&nbsp;Editar</a></td>
         <?php } ?>
 
     </tr> 
