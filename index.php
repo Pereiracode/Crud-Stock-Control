370427255f1ffe49bbc0beb1d4c74e0996074f29
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-03-20 20:25:27 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,75 +1,47 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Menu</title>
+    <style type="text/css">
+        #tamanho{
+            -webkit-box-shadow: 10px 10px 5px -2px rgba(163,158,163,0.32);
+            -moz-box-shadow: 10px 10px 5px -2px rgba(163,158,163,0.32);
+            box-shadow: 10px 10px 5px -2px rgba(163,158,163,0.32);
+        }
+            
+        
+    </style>
 </head>
 <body>
-    
-<div class="container" style="margin-top:100px">
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Produto</h5>
-        <p class="card-text">Opção para adicionar produtos em nosso estoque.</p>
-        <a href="adicionar_produto.php" class="btn btn-primary">Cadastrar Produto</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Lista de Produtos</h5>
-        <p class="card-text">Visualizar, editar e excluir produtos.</p>
-        <a href="listar_produtos.php" class="btn btn-primary">Listar</a>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-sm-6" style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Categoria</h5>
-        <p class="card-text">Opção para adicionar categorias de produtos.</p>
-        <a href="adicionar_categoria.php" class="btn btn-primary">Cadastrar Categoria</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6" style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Lista de Categorias</h5>
-        <p class="card-text">Visualizar, editar e excluir categorias.</p>
-        <a href="listar_categoria.php" class="btn btn-primary">Listar</a>
-      </div>
-    </div>
-  </div>
+<div class="container" id="tamanho" style="width:350px; margin-top:100px;border-radius:15px; border:2px solid #f3f3f3">
+    <div style="padding:15px">
+    <center>
+        <img src="img/cadeado.png" width="125px" heigth="125px" alt="Logo_login">
+    </center>
 
-  <div class="col-sm-6" style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Fornecedor</h5>
-        <p class="card-text">Opção para adicionar fornecedores.</p>
-        <a href="adicionar_fornecedor.php" class="btn btn-primary">Cadastrar Fornecedores</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6" style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Lista de Fornecedores</h5>
-        <p class="card-text">Visualizar, editar e excluir fornecedores.</p>
-        <a href="listar_fornecedor.php" class="btn btn-primary">Listar</a>
-      </div>
-    </div>
-  </div>
+    <form action="" method="post">
+        <div class="form-group">
+            <label >Usuário:</label>
+            <input type="text" name="usuario" class="form-control" placeholder="Usuário" autocomplete="off" required>
+        </div>
+        <div class="form-group">
+            <label >Senha:</label>
+            <input type="password" name="senha" class="form-control" placeholder="Senha" autocomplete="off" required>
+        </div>
+    </form>
 
+    <div style="text-align:right">
+        <button type="submit" class="btn btn-sm btn-success">Entrar</button>
+    </div>
+
+    </div>
 </div>
-</div>
+
+
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
